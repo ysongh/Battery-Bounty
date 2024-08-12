186fab8789c84aaea90f0326350a1d52d919406e
You Song Hou <ysongweb3@gmail.com>
2024-08-12 01:42:24 -0400

    feat: install 'react-router-dom'

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,14 +1,24 @@
 import { useState } from 'react';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
 
   return (
-    <>
-      <div>
-       <h1>Battery Bounty</h1>
-       <w3m-button />
-      </div>
-    </>
+    <HashRouter>
+        <Routes>
+          <Route
+            path="/test"
+            element={
+              <h1>Test</h1>} />
+          <Route
+            path="/"
+            element={
+              <>
+                <h1>Battery Bounty</h1>
+                <w3m-button />
+              </>} />
+        </Routes>
+      </HashRouter>
   );
 };
 
