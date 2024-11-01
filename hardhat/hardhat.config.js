d7078eef1a9568d7b4828e31c8c9e8762228eabb
You Song Hou <ysongweb3@gmail.com>
2024-10-31 23:06:30 -0400

    feat: add Morph Holesky Testnet RPC

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -4,6 +4,14 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    morphTestnet: {
+      url: process.env.MORPH_TESTNET_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      gasPrice: 2000000000 // 2 gwei in wei
+    },
+  },
   paths: {
     artifacts: '../reactjs/src/artifacts',
     cache: '../reactjs/src/cache',
